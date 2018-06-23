c9cb8d75621cd95d6d03eb9144f7ae0df8d308fa
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-06-23 22:22:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
-  if array.length >= 3
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else array.length >= 3
      array.join(", ").insert(-2, " and ")
   end
 
